347c399662d95cade6c3aab01b389e75abb3c53d
Mary <Mary.Bartlett@mixrad.io>
2016-02-28 16:36:43 +0000

    building up the rock paper scissors logic in the controller

--- client/app/features/rockPaperScissors/rockPaperScissors.controller.js
+++ client/app/features/rockPaperScissors/rockPaperScissors.controller.js
@@ -1,3 +1,38 @@
+const rockPaperScissors = ['rock', 'paper', 'scissors'];
+const l = rockPaperScissors.length;
+
+const determineWinner = function determineWinner (player1, player2) {
+
+    if (player1 === player2) {
+        return 'draw';
+    }
+
+    // this isn't at all neat - just trying to use plain JS here
+    // could use lodash - but still figuring out what data structure I want to use
+    // will probably change the function to use an object instead of the strings
+
+    if (player1  === 'rock' || player2 === 'rock') {
+        if (player1 === 'scissors' || player2 === 'scissors') {
+            return 'rock';
+        }
+        if (player1 === 'paper' || player2 === 'paper') {
+            return 'paper';
+        }
+    }
+
+    if (player1  === 'paper' || player2 === 'paper') {
+        if (player1  === 'scissors'|| player2 === 'scissors') {
+            return 'scissors';
+        }
+    }
+
+}
+
+const randomValue = function returnRandomValue () {
+    const randomPos = Math.round(Math.random() * (l - 1)); // subtract 1 due to arrays being zero indexed
+    return rockPaperScissors[randomPos];
+}
+
 
 export default class rockPaperScissorsController {
   static get UID(){
@@ -6,6 +41,7 @@ export default class rockPaperScissorsController {
   
   /* @ngInject */
   constructor() {
-    this.name = "Name" // this is just any value for the minute to assert testing works
+    this.winner = determineWinner,
+    this.randomValue = randomValue;
   }
 }
--- client/app/features/rockPaperScissors/rockPaperScissors.controller.spec.js
+++ client/app/features/rockPaperScissors/rockPaperScissors.controller.spec.js
@@ -1,16 +1,63 @@
 import rockPaperScissors from './index';
 
 describe('Controller: rockPaperScissors', function() {
-  let $controller;
+  let $controller, ctrl;
 
-  beforeEach(angular.mock.module(rockPaperScissors));
+    beforeEach(angular.mock.module(rockPaperScissors));
 
-  beforeEach(angular.mock.inject(function(_$controller_) {
-    $controller = _$controller_;
-  }));
+    beforeEach(angular.mock.inject(function(_$controller_) {
+        $controller = _$controller_;
+        ctrl = $controller('rockPaperScissorsController');
+    }));
 
-  it('name is initialized to Name', function() {
-    let ctrl = $controller('rockPaperScissorsController');
-    expect(ctrl.name).toBe('Name');
+
+    describe('determineWinner', function () {
+
+        it('returns \'draw\' if both values are the same', function() {
+            expect(ctrl.winner('rock', 'rock')).toEqual('draw');
+            expect(ctrl.winner('paper', 'paper')).toEqual('draw');
+            expect(ctrl.winner('scissors', 'scissors')).toEqual('draw');
+        });
+
+        it('rock beats scissors', function () {
+            expect(ctrl.winner('rock', 'scissors')).toEqual('rock');
+            expect(ctrl.winner('scissors', 'rock')).toEqual('rock');
+        });
+
+        it('paper beats rock', function () {
+            expect(ctrl.winner('rock', 'paper')).toEqual('paper');
+            expect(ctrl.winner('paper', 'rock')).toEqual('paper');
+        });
+
+        it('scissors beats paper', function () {
+            expect(ctrl.winner('paper', 'scissors')).toEqual('scissors');
+            expect(ctrl.winner('scissors', 'paper')).toEqual('scissors');
+        });
   });
+
+
+    describe('randomValue', function () {
+
+        it('returns rock when Math.random and Math.round results in 0', function () {
+            spyOn(Math, 'random').and.returnValue(0.0);
+            expect(ctrl.randomValue()).toEqual('rock');
+        });
+
+        it('returns paper when Math.random and Math.round results in 1', function () {
+            spyOn(Math, 'random').and.returnValue(0.3);
+            expect(ctrl.randomValue()).toEqual('paper');
+        });
+
+
+        it('returns paper when Math.random and Math.round results in 2', function () {
+            spyOn(Math, 'random').and.returnValue(0.8);
+            expect(ctrl.randomValue()).toEqual('scissors');
+        });
+
+        it('returns scissors when Math.random and Math.round results in 2', function () {
+            spyOn(Math, 'random').and.returnValue(1);
+            expect(ctrl.randomValue()).toEqual('scissors');
+        });
+
+    });
 });
